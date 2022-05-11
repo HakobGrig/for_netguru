04b91041ca4713b61118e47e25b49e8c8316b967
Hakob <hakob.grigoryan@teracloud.com>
2022-05-11 13:40:58 +0400

    Added process.exit in case of initialization failed.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import * as env from './env';
-env.init();
+
+// Init environment variables first
+try {
+  env.init();
+} catch (error) {
+  if (error instanceof Error) {
+    console.log('Unable to initialize env variables...', error.message);
+  }
+
+  // eslint-disable-next-line no-process-exit
+  process.exit(-1);
+}
 
 import * as storage from './storage';
 import * as server from './server';
@@ -9,7 +20,11 @@ async function init() {
   await server.init();
 }
 
+// Init storage and server
 init().catch(error => {
   console.log('Unable to start server...', error.message);
   console.log('Environment variables: \n', env.EnvironmentVariables);
+
+  // eslint-disable-next-line no-process-exit
+  process.exit(-1);
 });
